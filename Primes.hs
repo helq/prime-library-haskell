69b894e7eb2b9c8af7cc4b19c432b6911344f53a
Elkin Cruz <linuxero789@gmail.com>
2012-12-30 21:09:00 +0100

    Little improvements and documentation

--- Primes.hs
+++ Primes.hs
@@ -14,13 +14,17 @@ import Data.List (find)
 
 primes :: Integral a => [a]
 primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
-         ++ [x|x<-posible, isPrime' x]
-    where posible = f 101 $ cycle [2,4,2,4,6,2,6,4] -- no divisible for 2, 3 or 5
-            where f a (x:xs) = a : f (a+x) xs
+         ++ [x|x<-posibles, isPrime' x]
+
+    where posibles = 101 : zipWith (+) posibles
+                                      (cycle [2,4,2,4,6,2,6,4]) -- no divisible for 2, 3 or 5
+
+          primes' :: Integral a => [a]
+          primes' = drop 3 primes
 
           isPrime' :: Integral a => a -> Bool
           isPrime' n = not . any (divide n)
-                     . takeLeastSqrt n $ drop 3 primes
+                     . takeLeastSqrt n $ primes'
 
 
 isPrime :: Integral a => a -> Bool
@@ -36,7 +40,7 @@ sqrtIntegral = truncate . sqrt . fromIntegral
 takeLeastSqrt :: Integral a => a -> [a] -> [a]
 takeLeastSqrt n = takeWhile (<= sqrtIntegral n)
 
-factorPrimes :: Integral a => a -> [a]
+factorPrimes :: Integral a => a -> [a] -- Only for natural numbers greater than 1
 factorPrimes n
     | n < 2     = []
     | otherwise = factor : factorPrimes rest
@@ -46,7 +50,7 @@ factorPrimes n
           rest = n `div` factor
 
 intsInFactors :: Integral a => [(a,[a])]
-intsInFactors = zip [2..] $ list primes [2..]
-    where list (p:ps) (n:ns)
+intsInFactors = zip [2..] $ list primes [2..] -- list is list of factorPrimes of every
+    where list (p:ps) (n:ns)                  -- natural greater than 1
             | n < p     = factorPrimes n : list (p:ps) ns
             | otherwise = [p] : list ps ns
